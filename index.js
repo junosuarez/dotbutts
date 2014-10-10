c0a4b2dcd14ffdc5bec3d818c57ba8ab9744e4bb
jden <jason@denizac.org>
2014-10-10 01:46:42 -0400

    working ish cnames

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var dnsd = require('dnsd')
 var dgram = require('dgram')
 var domains = require('./domains')
+var dns = require('dns')
 
 var proxyServer = '8.8.8.8'
 
@@ -17,20 +18,74 @@ dnsd.createServer(function (req, res) {
       // console.log('resolving', domain, type, start, name)
       
 
+      function resolveRecord(all, start, name, type) {
+        console.log('r', start, name, type)
+        if (!endsIn(start, '.butts')) { console.log('s',start); return null}
+        try {
+          var answer = all[start][type].filter(function (el) {
+            return el.name === name
+            }).slice(0,1)[0]
+          cosnsole.log(start, name, type, answer)
+        } catch (e) {}
+          if (answer) {
+            var x = [{
+              name: (name == '@' ? '' : name + '.') + start,
+              type: type,
+              data: answer.ip || answer.alias,
+              ttl: 900
+            }]
+            console.log('->', x)
+            return x
+          } else if (type === 'CNAME') {
+            return null
+          } else {
+            var cname = resolveRecord(all, start, name, 'CNAME')[0]
+            console.log('cname', cname)
+            if (!cname) { return null }
+            if (!endsIn(cname.data, '.butts.')) {
+              // beyond our authority, delegate!
+              return [cname]
+            }
+            return [cname].concat(
+                resolveRecord(all, cname.data.substr(0, cname.data.length-1), '@', type)
+              )
+            // return resolveRecord(all, start, name,)
+          }
+      }
+
     domains(function (err, all) {
       if (err) {console.error(err); res.end() }
       // console.log(all[start][type])
       try {
-        var answer = all[start][type].filter(function (el) {
-          return el.name === name
-        }).slice(0,1)[0] || {}
-       
-        if (answer.ip) { return res.end(answer.ip) }
-        if (answer.alias) { return res.end(answer.alias) }
-        res.end()
+        var answer = resolveRecord(all, start, name, type)
+        
+        if (!answer) {
+          return res.end()
+        }
+
+        answer.forEach(function (response) {
+          res.answer.push(response)
+        })
+        
+        if (answer[answer.length-1].type === 'CNAME' && type !== 'CNAME') {
+          // delegate to other server
+          console.log('resolve', answer[answer.length-1].data, type)
+          dns.resolve(answer[answer.length-1].data, type, function (err, result) {
+            if (err) { console.error(err); return res.end() }
+            console.log('resolved', result)
+            result.forEach(function (response) {
+              res.answer.push({type: 'A', name: answer[answer.length-1].data, data: response, ttl: 900})
+            })
+            res.end()
+
+          })
+        } else {
+          return res.end()
+        }
+
 
       } catch (e) {
-        console.log(e)
+        console.error(e)
         return res.end()
       }
     })
@@ -48,6 +103,8 @@ function endsIn(str, suffix) {
   return str.substr(str.length - suffix.length) === suffix
 }
 require('assert')(endsIn('foo.butts','.butts'))
+
+
 
 function proxy(req, res) {
   // hats off to mafintosh's dnsjack
